5cb8efdb5ffba499da2e660bcad48a8e2ae5cc49
Nicolae-Andrei Cuconoiu <andreicuconoiu@gmail.com>
2025-04-23 20:20:30 +0300

    feat: Middleware to check if user is authenticated

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const express = require("express");
 const login = require("./src/routes/login.route");
 const auth = require("./src/routes/auth.route");
 const apiConfig = require("./src/config/api.config");
+const authenticatedUser = require("./src/middleware/auth_user.middleware");
 
 const app = express();
 
@@ -17,6 +18,8 @@ app.use(cors()).use(cookieParser());
 
 app.use("/login", login);
 app.use("/auth", auth);
+
+app.use(authenticatedUser);
 
 app.listen(apiConfig.APP_PORT, () => {
   console.log(`Server listening at port ${apiConfig.APP_PORT}`);
